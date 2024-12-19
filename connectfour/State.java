95f167e8595a92048c419ee2820b41c22c441fac
Leti <138636041+Sndx27@users.noreply.github.com>
2024-12-19 18:04:39 +0700

    Frist Commit

---
+++ connectfour/State.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connectfour;
+
+/**
+ *
+ * @author Clay Amsal
+ */
+public enum State {  
+   PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
