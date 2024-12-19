ef1c11f9ae430235552281195d1c4331d78cda13
burjuharianja <burjuferdinand09@gmail.com>
2024-12-20 01:37:24 +0700

    upd: welcome screen :3

--- src/tictactoegui/GameMain.java
+++ src/tictactoegui/GameMain.java
@@ -114,20 +114,17 @@ public class GameMain extends JPanel {
    }
 
    /** The entry "main" method */
-   public void play(){
-      // Run GUI construction codes in Event-Dispatching thread for thread safety
-      javax.swing.SwingUtilities.invokeLater(new Runnable() {
-         public void run() {
-            JFrame frame = new JFrame(TITLE);
-            // Set the content-pane of the JFrame to an instance of main JPanel
-            frame.setContentPane(new GameMain());
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.pack();
-            frame.setLocationRelativeTo(null); // center the application window
-            frame.setVisible(true);            // show it
-         }
+   public static void play() {
+      javax.swing.SwingUtilities.invokeLater(() -> {
+          JFrame frame = new JFrame(TITLE);
+          frame.setContentPane(new GameMain());
+          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          frame.pack();
+          frame.setLocationRelativeTo(null);
+          frame.setVisible(true);
       });
-   }
+  }
+  
 }
 
    
